70437a56fb6b70d49335bd60d25c67468669d052
lvshubiao <lvshubiao@bonc.com.cn>
2018-06-06 11:15:32 +0800

    提取redux

---
+++ actions/types.js
@@ -0,0 +1,8 @@
+
+// action type 定义
+//收账
+export const ACTION_TYPE_GET_BILL = 'ACTION_TYPE_GET_BILL';
+//分润
+export const ACTION_TYPE_GET_PROFIT = 'ACTION_TYPE_GET_PROFIT';
+
+
---
+++ constants/index.js
@@ -0,0 +1,11 @@
+
+
+/*
+ * 常量定义
+ * 备注： 存储到localstrage的key不要出现下划线
+ */
+
+'use strict';
+export const ACTION_LOADINGD = 'ACTION_LOADINGD';
+export const ACTION_SUCCESS  = 'ACTION_SUCCESS';
+export const ACTION_FAILED   = 'ACTION_FAILED';
