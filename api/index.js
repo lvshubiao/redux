722eef7938542f43e2cc0b8fc480eef038565f80
lvshubiao <lvshubiao@bonc.com.cn>
2018-06-06 11:32:52 +0800

    .

--- api/index.js
+++ api/index.js
@@ -6,10 +6,5 @@
 
 'use strict';
 
-export * from './bankcard';
 export * from './bill';
-export * from './certificate';
-export * from './payment';
-export * from './qrcode';
-export * from './user';
-export * from './cashout';
+
